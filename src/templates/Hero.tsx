dd5c234c4c20e5a8aaa49417d7f290a940d4b0f1
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 21:37:56 -0700

    update

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -31,7 +31,8 @@ const Hero = () => (
         button={
           <Link href="https://creativedesignsguru.com/category/nextjs/">
             <a>
-              <Button xl>Get Started</Button>
+              {/* Add custom class 'mainButton' to the Button component */}
+              <Button xl className="mainButton">Get Started</Button>
             </a>
           </Link>
         }
@@ -41,3 +42,45 @@ const Hero = () => (
 );
 
 export { Hero };
+
+// Add the following CSS to your styles.css file:
+
+/*
+.mainButton {
+  padding: 2rem 4rem;
+  font-size: 2rem;
+  height: 10rem;
+}
+
+@media (min-width: 640px) {
+  .mainButton {
+    padding: 1.5rem 3rem;
+    font-size: 1.5rem;
+    height: 8rem;
+  }
+}
+
+@media (min-width: 768px) {
+  .mainButton {
+    padding: 1rem 2rem;
+    font-size: 1.25rem;
+    height: 6rem;
+  }
+}
+
+@media (min-width: 1024px) {
+  .mainButton {
+    padding: 0.75rem 1.5rem;
+    font-size: 1rem;
+    height: 4rem;
+  }
+}
+
+@media (min-width: 1280px) {
+  .mainButton {
+    padding: 0.5rem 1rem;
+    font-size: 0.875rem;
+    height: 3rem;
+  }
+}
+*/
