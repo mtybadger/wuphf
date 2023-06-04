6b6b86043bdab0162e63ae79ad342fd6074a4342
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 19:37:36 -0700

    update

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -7,6 +7,22 @@ import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
+// Function to replace dog-related text with cat-related text
+const replaceDogTextWithCatText = (text) => {
+  const dogToCat = {
+    "dog": "cat",
+    "Dog": "Cat",
+    "puppy": "kitten",
+    "Puppy": "Kitten",
+    "canine": "feline",
+    "Canine": "Feline",
+  };
+
+  const regex = new RegExp(Object.keys(dogToCat).join("|"), "gi");
+
+  return text.replace(regex, (matched) => dogToCat[matched]);
+};
+
 const Hero = () => (
   <Background color="bg-gray-100">
     <Section yPadding="py-6">
@@ -23,11 +39,11 @@ const Hero = () => (
       <HeroOneButton
         title={
           <>
-            {'The revolutionary AI-powered device\n'}
-            <span className="text-primary-500">for talking to your dog</span>
+            {replaceDogTextWithCatText('The revolutionary AI-powered device\n')}
+            <span className="text-primary-500">{replaceDogTextWithCatText('for talking to your dog')}</span>
           </>
         }
-        description="Wuphf translates your dog's barks into human language, finally bridging the communication gap between you and your furry friend."
+        description={replaceDogTextWithCatText("Wuphf translates your dog's barks into human language, finally bridging the communication gap between you and your furry friend.")}
         button={
           <Link href="https://creativedesignsguru.com/category/nextjs/">
             <a>
