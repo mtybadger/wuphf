b4936d6cc476a03b0a9643b66ae067a9988d02e6
Spruce Campbell <spruce@mit.edu>
2023-06-04 11:00:34 -0700

    Revert "Button fix"
    This reverts commit 86ad2ebc3a3774d21300f94210a310d3cdfad729.

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,12 +1,13 @@
+
 type IButtonProps = {
   xl?: boolean;
   children: string;
 };
 
 const Button = (props: IButtonProps) => {
-  // Use the blue-500 color for the button background
+
   return (
-    <div className="text-white bg-blue-500 inline-block rounded-md text-center font-extrabold text-xl py-4 px-6">
+    <div className="text-white bg-primary-500 inline-block rounded-md text-center font-extrabold text-xl py-4 px-6">
       {props.children}
     </div>
   );
