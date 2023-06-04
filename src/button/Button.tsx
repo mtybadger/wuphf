a1096e91d783a5ac99493dcabf96c88091353842
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-04 10:55:21 -0700

    update

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,15 +1,20 @@
+```tsx
+// Import the necessary Tailwind CSS classes
+import 'tailwindcss/tailwind.css';
+
 type IButtonProps = {
   xl?: boolean;
   children: string;
 };
 
 const Button = (props: IButtonProps) => {
-  // Use the blue-500 color for the button background
+  // Replace the blue-500 color with brown-500 for the button background
   return (
-    <div className="text-white bg-blue-500 inline-block rounded-md text-center font-extrabold text-xl py-4 px-6">
+    <div className="text-white bg-brown-500 inline-block rounded-md text-center font-extrabold text-xl py-4 px-6">
       {props.children}
     </div>
   );
 };
 
 export { Button };
+```
