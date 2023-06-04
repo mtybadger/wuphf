027a11e4d33a13e2637892bb0d5765ca525a5007
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 19:21:36 -0700

    update

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -6,15 +6,32 @@ type IHeroOneButtonProps = {
   button: ReactNode;
 };
 
-const HeroOneButton = (props: IHeroOneButtonProps) => (
-  <header className="text-center">
-    <h1 className="text-5xl text-gray-900 font-bold whitespace-pre-line leading-hero">
-      {props.title}
-    </h1>
-    <div className="text-2xl mt-4 mb-16">{props.description}</div>
+const HeroOneButton = (props: IHeroOneButtonProps) => {
+  // Replace dog-related text with equivalent cat-related text
+  const replaceDogTextWithCatText = (text: string) => {
+    const dogToCatMap = {
+      dog: 'cat',
+      puppy: 'kitten',
+      canine: 'feline',
+    };
 
-    {props.button}
-  </header>
-);
+    const regex = new RegExp(Object.keys(dogToCatMap).join('|'), 'gi');
+    return text.replace(regex, (matched) => dogToCatMap[matched.toLowerCase()]);
+  };
+
+  const updatedTitle = replaceDogTextWithCatText(props.title);
+  const updatedDescription = replaceDogTextWithCatText(props.description);
+
+  return (
+    <header className="text-center">
+      <h1 className="text-5xl text-gray-900 font-bold whitespace-pre-line leading-hero">
+        {updatedTitle}
+      </h1>
+      <div className="text-2xl mt-4 mb-16">{updatedDescription}</div>
+
+      {props.button}
+    </header>
+  );
+};
 
 export { HeroOneButton };
