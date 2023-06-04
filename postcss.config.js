1dd720ef64367720facf29b56b3fa02f031de8e2
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 21:31:44 -0700

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
+// No changes are needed in the postcss.config.js file for the given task.
+// The task is related to creating a theme context and provider, which does not involve modifying the PostCSS configuration.
+
+// Returning the original file:
+
 // Please do not use the array form (like ['tailwindcss', 'postcss-preset-env'])
 // it will create an unexpected error: Invalid PostCSS Plugin found: [0]
 
