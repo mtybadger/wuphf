740a5cf454bbaa9ef6d3008cf234a0d866374e13
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 19:21:37 -0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,56 @@
 import { AppProps } from 'next/app';
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '../styles/global.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const replaceDogTextWithCatText = (text: string) => {
+  const dogTerms = [
+    { dog: 'dog', cat: 'cat' },
+    { dog: 'puppy', cat: 'kitten' },
+    { dog: 'canine', cat: 'feline' },
+  ];
+
+  let newText = text;
+  dogTerms.forEach((term) => {
+    newText = newText.replace(new RegExp(term.dog, 'gi'), term.cat);
+  });
+
+  return newText;
+};
+
+const replaceDogImagesWithCatImages = (element: HTMLElement) => {
+  const dogImages = element.querySelectorAll('img[src*="dog"], img[src*="puppy"], img[src*="canine"]');
+
+  dogImages.forEach((img) => {
+    const src = img.getAttribute('src');
+    if (src) {
+      img.setAttribute('src', src.replace(/(dog|puppy|canine)/gi, 'cat'));
+    }
+  });
+};
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (typeof window !== 'undefined') {
+        const body = document.querySelector('body');
+        if (body) {
+          body.innerHTML = replaceDogTextWithCatText(body.innerHTML);
+          replaceDogImagesWithCatImages(body);
+        }
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
