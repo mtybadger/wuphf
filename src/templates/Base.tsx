19bd19f83ad6947c33a9afd49e33800b0dee081e
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 21:31:47 -0700

    update

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,18 +1,28 @@
+```tsx
+// src/templates/Base.tsx
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
+import { ThemeProvider } from '../context/ThemeContext'; // Import ThemeProvider
+import { ToggleThemeButton } from '../components/ToggleThemeButton'; // Import ToggleThemeButton
 
 const Base = () => (
-  <div className="antialiased text-gray-600">
-    <Meta title={AppConfig.title} description={AppConfig.description} />
-    <Hero />
-    <VerticalFeatures />
-    <Banner />
-    <Footer />
-  </div>
+  <ThemeProvider> {/* Wrap the app with ThemeProvider */}
+    <div className="antialiased text-gray-600">
+      <Meta title={AppConfig.title} description={AppConfig.description} />
+      <ToggleThemeButton /> {/* Add the ToggleThemeButton component */}
+      <Hero />
+      <VerticalFeatures />
+      <Banner />
+      <Footer />
+    </div>
+  </ThemeProvider>
 );
 
 export { Base };
+```
+
+I have updated the `src/templates/Base.tsx` file to include the `ThemeProvider` and `ToggleThemeButton` components. The `ThemeProvider` wraps the entire app to provide the theme context, and the `ToggleThemeButton` component is added to allow users to toggle between light and dark themes.
