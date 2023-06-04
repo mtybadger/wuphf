fb8e5e2a5626f12b22e8260d02ec9d31415e7176
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-04 10:55:31 -0700

    update

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -1,3 +1,8 @@
+Since the task requires changing the color palette, we need to update the components used in the Banner.tsx file. Specifically, we need to update the Button and CTABanner components to use the new brown color palette.
+
+Here's the updated Banner.tsx file:
+
+```tsx
 import Link from 'next/link';
 
 import { Button } from '../button/Button';
@@ -12,12 +17,21 @@ const Banner = () => (
       button={
         <Link href="https://creativedesignsguru.com/category/nextjs/">
           <a>
-            <Button>Get Started</Button>
+            {/* Add the custom brown color class to the Button component */}
+            <Button className="bg-brown-500 hover:bg-brown-600">
+              Get Started
+            </Button>
           </a>
         </Link>
       }
+      // Add the custom brown color classes to the CTABanner component
+      titleClassName="text-brown-800"
+      subtitleClassName="text-brown-700"
     />
   </Section>
 );
 
 export { Banner };
+```
+
+In this updated file, we added the custom brown color classes to the Button and CTABanner components. The Button component now has the "bg-brown-500" and "hover:bg-brown-600" classes, and the CTABanner component has the "text-brown-800" and "text-brown-700" classes for the title and subtitle, respectively.
