86ad2ebc3a3774d21300f94210a310d3cdfad729
Spruce Campbell <spruce@mit.edu>
2023-06-03 20:47:02 -0700

    Button fix

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,4 +1,3 @@
-import className from 'classnames';
 
 type IButtonProps = {
   xl?: boolean;
@@ -6,40 +5,10 @@ type IButtonProps = {
 };
 
 const Button = (props: IButtonProps) => {
-  const btnClass = className({
-    btn: true,
-    'btn-xl': props.xl,
-    'btn-base': !props.xl,
-    'btn-primary': true,
-  });
 
   return (
-    <div className={btnClass}>
+    <div className="text-white bg-primary-800 inline-block rounded-md text-center font-extrabold text-xl py-4 px-6">
       {props.children}
-
-      <style jsx>
-        {`
-          .btn {
-            @apply inline-block rounded-md text-center;
-          }
-
-          .btn-base {
-            @apply text-lg font-semibold py-2 px-4;
-          }
-
-          .btn-xl {
-            @apply font-extrabold text-xl py-4 px-6;
-          }
-
-          .btn-primary {
-            @apply text-white bg-primary-800;
-          }
-
-          .btn-primary:hover {
-            @apply bg-primary-900;
-          }
-        `}
-      </style>
     </div>
   );
 };
