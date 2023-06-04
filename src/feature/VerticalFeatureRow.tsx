d6dafbd64a7ab8dd2d7f61f5f9c759fddbe2039a
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 19:21:35 -0700

    update

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -22,15 +22,33 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
 
   const router = useRouter();
 
+  // Replace dog-related text with cat-related text
+  const updatedTitle = props.title.replace(/dog|puppy|canine/gi, (matched) => {
+    return matched === 'dog' ? 'cat' : matched === 'puppy' ? 'kitten' : 'feline';
+  });
+
+  const updatedDescription = props.description.replace(/dog|puppy|canine/gi, (matched) => {
+    return matched === 'dog' ? 'cat' : matched === 'puppy' ? 'kitten' : 'feline';
+  });
+
+  // Replace dog images with cat images
+  const updatedImage = props.image.replace(/dog|puppy|canine/gi, (matched) => {
+    return matched === 'dog' ? 'cat' : matched === 'puppy' ? 'kitten' : 'feline';
+  });
+
+  const updatedImageAlt = props.imageAlt.replace(/dog|puppy|canine/gi, (matched) => {
+    return matched === 'dog' ? 'cat' : matched === 'puppy' ? 'kitten' : 'feline';
+  });
+
   return (
     <div className={verticalFeatureClass}>
       <div className="w-full sm:w-1/2 text-center sm:px-6">
-        <h3 className="text-3xl text-gray-900 font-semibold">{props.title}</h3>
-        <div className="mt-6 text-xl leading-9">{props.description}</div>
+        <h3 className="text-3xl text-gray-900 font-semibold">{updatedTitle}</h3>
+        <div className="mt-6 text-xl leading-9">{updatedDescription}</div>
       </div>
 
       <div className="w-full sm:w-1/2 p-6">
-        <img src={`${router.basePath}${props.image}`} alt={props.imageAlt} />
+        <img src={`${router.basePath}${updatedImage}`} alt={updatedImageAlt} />
       </div>
     </div>
   );
