08d98ca3e69a02be77f6a923c3b806489dbd12c4
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 21:31:45 -0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import { AppProps } from 'next/app';
+import { ThemeProvider } from '../components/ThemeContext';
 
 import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <ThemeProvider>
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
 
 export default MyApp;
