8218695e894e2695b1596136d8da399245e8a77e
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 21:20:09 -0700

    update

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,11 +1,12 @@
-
 type IButtonProps = {
   xl?: boolean;
   children: string;
 };
 
 const Button = (props: IButtonProps) => {
-
+  // No changes needed in the Button component as the primary color is being
+  // updated in the tailwind.config.js file, which will automatically apply
+  // the new color to the 'bg-primary-800' class used in this component.
   return (
     <div className="text-white bg-primary-800 inline-block rounded-md text-center font-extrabold text-xl py-4 px-6">
       {props.children}
