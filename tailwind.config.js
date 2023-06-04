68e46694b4532cc08113df99d29ed44c66b0e5ae
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-04 11:04:28 -0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,19 +15,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        // Replace the primary color palette with the desired blue shades
-        primary: {
-          50: '#f0f9ff',
-          100: '#e0f2fe',
-          200: '#bae6fd',
-          300: '#7dd3fc',
-          400: '#38bdf8',
-          500: '#0ea5e9',
-          600: '#0284c7',
-          700: '#0369a1',
-          800: '#075985',
-          900: '#0c4a6e',
-        },
+        // Replace the primary color palette with the desired brown color
+        primary: '#654321',
         gray: {
           100: '#f7fafc',
           200: '#edf2f7',
