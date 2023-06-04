6758d6a8e8a57ebcdb392212a03e5810b7af043e
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 21:37:55 -0700

    update

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -12,7 +12,8 @@ const Banner = () => (
       button={
         <Link href="https://creativedesignsguru.com/category/nextjs/">
           <a>
-            <Button>Get Started</Button>
+            {/* Add the custom class "mainButton" to the Button component */}
+            <Button className="mainButton">Get Started</Button>
           </a>
         </Link>
       }
@@ -21,3 +22,31 @@ const Banner = () => (
 );
 
 export { Banner };
+
+// In your styles.css or a new CSS file, add the following styles:
+
+/*
+.mainButton {
+  padding: 2rem 4rem;
+  font-size: 2rem;
+  height: 10rem;
+}
+
+@media (min-width: 640px) {
+  .mainButton {
+    padding: 1.5rem 3rem;
+    font-size: 1.5rem;
+    height: 8rem;
+  }
+}
+
+@media (min-width: 768px) {
+  .mainButton {
+    padding: 1rem 2rem;
+    font-size: 1.25rem;
+    height: 6rem;
+  }
+}
+
+// And so on for lg, xl, and 2xl breakpoints
+*/
