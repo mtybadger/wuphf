bd5ae02eb9c96c189efa092a96c185b606ef7a29
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 21:37:53 -0700

    update

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -13,8 +13,39 @@ const HeroOneButton = (props: IHeroOneButtonProps) => (
     </h1>
     <div className="text-2xl mt-4 mb-16">{props.description}</div>
 
-    {props.button}
+    {/* Add the mainButton class to the button */}
+    <div className="mainButton">
+      {props.button}
+    </div>
   </header>
 );
 
 export { HeroOneButton };
+
+// In your styles.css file, add the following styles for the mainButton class:
+
+/*
+.mainButton {
+  padding: 2rem 4rem;
+  font-size: 2rem;
+  height: 10rem;
+}
+
+@media (min-width: 640px) {
+  .mainButton {
+    padding: 1.5rem 3rem;
+    font-size: 1.5rem;
+    height: 8rem;
+  }
+}
+
+@media (min-width: 768px) {
+  .mainButton {
+    padding: 1rem 2rem;
+    font-size: 1.25rem;
+    height: 6rem;
+  }
+}
+
+// And so on for lg, xl, and 2xl breakpoints
+*/
