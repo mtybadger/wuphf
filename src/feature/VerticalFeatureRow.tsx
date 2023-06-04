2f89b03a437105cd3733f743330d2fb63270e908
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 19:37:30 -0700

    update

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -1,3 +1,8 @@
+Since the task involves modifying the text content of the landing page, we need to make changes to the `VerticalFeatureRow` component to replace dog-related text with cat-related text. We will implement the plan by adding the `find_dog_text` and `replace_text` functions and modifying the `title` and `description` props before rendering them.
+
+Here's the updated code:
+
+```javascript
 import className from 'classnames';
 import { useRouter } from 'next/router';
 
@@ -7,6 +12,36 @@ type IVerticalFeatureRowProps = {
   image: string;
   imageAlt: string;
   reverse?: boolean;
+};
+
+// Function to find dog-related text in the content
+const find_dog_text = (page_content: string) => {
+  const dog_words = ["dog", "puppy", "canine"];
+  const dog_text_locations = [];
+
+  dog_words.forEach((word) => {
+    let index = page_content.indexOf(word);
+    while (index !== -1) {
+      dog_text_locations.push({ word, index });
+      index = page_content.indexOf(word, index + 1);
+    }
+  });
+
+  return dog_text_locations;
+};
+
+// Function to replace text in the content based on the provided replacements
+const replace_text = (page_content: string, replacements: any[]) => {
+  let modified_content = page_content;
+
+  replacements.forEach((replacement) => {
+    modified_content = modified_content.replace(
+      replacement.word,
+      replacement.new_text
+    );
+  });
+
+  return modified_content;
 };
 
 const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
@@ -22,11 +57,34 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
 
   const router = useRouter();
 
+  // Find dog-related text in the title and description
+  const dog_text_title = find_dog_text(props.title);
+  const dog_text_description = find_dog_text(props.description);
+
+  // Replace dog-related text with cat-related text
+  const dog_to_cat = { dog: "cat", puppy: "kitten", canine: "feline" };
+  const title_replacements = dog_text_title.map((item) => ({
+    ...item,
+    new_text: dog_to_cat[item.word],
+  }));
+  const description_replacements = dog_text_description.map((item) => ({
+    ...item,
+    new_text: dog_to_cat[item.word],
+  }));
+
+  const modified_title = replace_text(props.title, title_replacements);
+  const modified_description = replace_text(
+    props.description,
+    description_replacements
+  );
+
   return (
     <div className={verticalFeatureClass}>
       <div className="w-full sm:w-1/2 text-center sm:px-6">
-        <h3 className="text-3xl text-gray-900 font-semibold">{props.title}</h3>
-        <div className="mt-6 text-xl leading-9">{props.description}</div>
+        <h3 className="text-3xl text-gray-900 font-semibold">
+          {modified_title}
+        </h3>
+        <div className="mt-6 text-xl leading-9">{modified_description}</div>
       </div>
 
       <div className="w-full sm:w-1/2 p-6">
@@ -37,3 +95,6 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
 };
 
 export { VerticalFeatureRow };
+```
+
+In this updated code, we have added the `find_dog_text` and `replace_text` functions and used them to modify the `title` and `description` props before rendering them in the component. This will replace any dog-related text with equivalent cat-related text on the landing page.
