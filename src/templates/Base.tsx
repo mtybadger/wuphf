dac5ae06b3b45dd044f43e253cf993ff03792647
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-04 10:55:33 -0700

    update

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,3 +1,6 @@
+Since the task requires changing the color palette to brown, we need to update the Tailwind CSS classes used in the components. Here's the updated Base.tsx file with the necessary changes:
+
+```tsx
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
@@ -6,7 +9,7 @@ import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
 
 const Base = () => (
-  <div className="antialiased text-gray-600">
+  <div className="antialiased text-brown-800"> {/* Updated text color */}
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Hero />
     <VerticalFeatures />
@@ -16,3 +19,8 @@ const Base = () => (
 );
 
 export { Base };
+```
+
+Now, you need to update the color classes in the components used in Base.tsx. For example, if the Hero component has a background color of `bg-blue-500`, you should replace it with `bg-brown-500`. Similarly, update the color classes in the Banner, Footer, and VerticalFeatures components.
+
+Remember to update the tailwind.config.js file as mentioned in the proposed solution to add the brown color palette.
