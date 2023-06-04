f281781c07a82c44738470bcac5a85c060fafb3f
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 19:21:38 -0700

    update

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -4,20 +4,26 @@ import { Button } from '../button/Button';
 import { CTABanner } from '../cta/CTABanner';
 import { Section } from '../layout/Section';
 
-const Banner = () => (
-  <Section>
-    <CTABanner
-      title="Stop sounding so dumb to your dog."
-      subtitle="Start your Free Trial."
-      button={
-        <Link href="https://creativedesignsguru.com/category/nextjs/">
-          <a>
-            <Button>Get Started</Button>
-          </a>
-        </Link>
-      }
-    />
-  </Section>
-);
+const Banner = () => {
+  // Replace dog-related text with equivalent cat-related text
+  const title = "Stop sounding so dumb to your dog.".replace("dog", "cat");
+  const subtitle = "Start your Free Trial.";
+
+  return (
+    <Section>
+      <CTABanner
+        title={title}
+        subtitle={subtitle}
+        button={
+          <Link href="https://creativedesignsguru.com/category/nextjs/">
+            <a>
+              <Button>Get Started</Button>
+            </a>
+          </Link>
+        }
+      />
+    </Section>
+  );
+};
 
 export { Banner };
