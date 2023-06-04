0805f2b7a9292c59a9f2dcff939a13ddce6eff61
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-04 10:55:40 -0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+```js
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -15,6 +16,18 @@ module.exports = {
     },
     extend: {
       colors: {
+        // Add the brown color palette
+        brown: {
+          100: '#f2e6e1',
+          200: '#e6d3c6',
+          300: '#d9bfab',
+          400: '#ccab91',
+          500: '#bf987e',
+          600: '#a17e6b',
+          700: '#846159',
+          800: '#5b463e',
+          900: '#3e2d29'
+        },
         // Replace the primary color palette with the desired blue shades
         primary: {
           50: '#f0f9ff',
@@ -47,3 +60,6 @@ module.exports = {
   },
   plugins: [],
 };
+```
+
+Now you can use the brown color palette in your project by replacing the color utility classes and values as mentioned in the proposed solution.
