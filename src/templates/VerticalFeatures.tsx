711a6cf5ae8b64f0265b3d314123d2584d641162
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-03 19:21:41 -0700

    update

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -4,26 +4,26 @@ import { Section } from '../layout/Section';
 const VerticalFeatures = () => (
 
   <Section
-    title="Communicate with Your Canine Companion"
-    description="Finally, understand what your dog is trying to say with Woof.ai. Our groundbreaking AI-powered device translates your dog's barks into human language, bringing you closer to your furry friend."
+    title="Communicate with Your Feline Companion"
+    description="Finally, understand what your cat is trying to say with Meow.ai. Our groundbreaking AI-powered device translates your cat's meows into human language, bringing you closer to your furry friend."
   >
     <VerticalFeatureRow
-      title="Real-Time Dog-to-Human Translation"
-      description="With Woof.ai, you'll never be left wondering what your dog wants. Our advanced AI algorithms analyze your dog's barks and provide instant translations in real-time, letting you understand their needs and desires."
-      image="/assets/images/dog1.svg"
-      imageAlt="Real-Time Dog-to-Human Translation"
+      title="Real-Time Cat-to-Human Translation"
+      description="With Meow.ai, you'll never be left wondering what your cat wants. Our advanced AI algorithms analyze your cat's meows and provide instant translations in real-time, letting you understand their needs and desires."
+      image="/assets/images/cat1.svg"
+      imageAlt="Real-Time Cat-to-Human Translation"
     />
     <VerticalFeatureRow
       title="Enhanced Bonding and Understanding"
-      description="Woof.ai strengthens the bond between you and your dog by facilitating clear communication. By knowing exactly what your dog is trying to communicate, you can better address their needs, leading to a happier and more fulfilling relationship."
-      image="/assets/images/dog2.svg"
+      description="Meow.ai strengthens the bond between you and your cat by facilitating clear communication. By knowing exactly what your cat is trying to communicate, you can better address their needs, leading to a happier and more fulfilling relationship."
+      image="/assets/images/cat2.svg"
       imageAlt="Enhanced Bonding and Understanding"
       reverse
     />
     <VerticalFeatureRow
       title="Tailored Training and Behavior Insights"
-      description="Gain valuable insights into your dog's behavior patterns with Woof.ai. Our intelligent system analyzes the translated barks and provides tailored training recommendations and behavior insights, helping you address any challenges or improve their training regimen."
-      image="/assets/images/dog3.svg"
+      description="Gain valuable insights into your cat's behavior patterns with Meow.ai. Our intelligent system analyzes the translated meows and provides tailored training recommendations and behavior insights, helping you address any challenges or improve their training regimen."
+      image="/assets/images/cat3.svg"
       imageAlt="Tailored Training and Behavior Insights"
     />
   </Section>
