1ad31b21023f823ae74739ac19ef0e2c27950ee1
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-19 03:36:52 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+```javascript
 import { Base } from '../templates/Base';
 
-const Index = () => <Base />;
+// Update the Base component to receive a title prop
+const Index = () => <Base title="Hello World" />;
 
 export default Index;
+```
+
+In order to update the title of the front page, we need to pass the new title as a prop to the `Base` component. In this case, we pass the title "Hello World" to the `Base` component. You will also need to update the `Base` component to accept and use the `title` prop for the `<title>` HTML tag.
