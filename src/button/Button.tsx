83ebfc7e9d45decb500d6d751e44cfb3bdabbae7
Spruce Campbell <67084031+mtybadger@users.noreply.github.com>
2023-06-19 03:54:09 +0100

    update

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,13 +1,16 @@
-
 type IButtonProps = {
   xl?: boolean;
   children: string;
 };
 
 const Button = (props: IButtonProps) => {
+  // Add a conditional class for the xl prop
+  const buttonSizeClass = props.xl ? 'text-2xl' : 'text-xl';
 
   return (
-    <div className="text-white bg-primary-800 inline-block rounded-md text-center font-extrabold text-xl py-4 px-6">
+    <div
+      className={`text-white bg-primary-800 inline-block rounded-md text-center font-extrabold py-4 px-6 ${buttonSizeClass}`}
+    >
       {props.children}
     </div>
   );
